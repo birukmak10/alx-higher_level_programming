3b0d62391ed5d52e55b1530e76ca4c5ea07d52f1
birukmak10 <birukmekonen10@gmail.com>
2023-05-16 12:26:59 +0300

    SQL_Introduction

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,3 @@
---Insert into
-INSERT INTO hbtn_0c_0.first_table (id, `name`) VALUES("89", "Best School");
+-- Insert values
+-- Execute: cat 7-insert_value.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
