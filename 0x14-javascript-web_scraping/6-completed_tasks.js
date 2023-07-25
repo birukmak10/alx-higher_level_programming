39bfc6d85f72e70d54f065159aeb86b97f4189e4
birukmak10 <birukmekonen10@gmail.com>
2023-07-25 17:15:41 +0300

    update

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,22 +1,22 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = process.argv[2];
-const list = {};
-request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    const abc = JSON.parse(body);
-    abc.forEach(function (item, index, array) {
-      if (item.completed) {
-        if (list[item.userId] === undefined) {
-          list[item.userId] = 1;
-        } else {
-          list[item.userId]++;
+
+// The first argument is the API URL
+const baseURL = process.argv[2];
+request(baseURL, (error, response, body) => {
+    const aggregate = {};
+    if (error) {
+        console.log(error);
+    }
+    const json = JSON.parse(body);
+    json.forEach(element => {
+        if (element.completed) {
+            if (!aggregate[element.userId]) {
+                aggregate[element.userId] = 0;
+            }
+            aggregate[element.userId]++;
         }
-      }
     });
-    console.log(list);
-  }
+    console.log(aggregate);
 });
